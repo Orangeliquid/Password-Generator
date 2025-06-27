07c2bac86e372c25297ef9a45cd10b104776bc51
Orangeliquid <hardym2020111@gmail.com>
2025-06-27 00:19:17 -0400

    remove include_uppercase_letters and include_lowercase_letters from class attributes

--- models/password_options.py
+++ models/password_options.py
@@ -3,8 +3,6 @@ from pydantic import BaseModel, Field, field_validator
 
 class PasswordOptions(BaseModel):
     length: int = Field(12, ge=8, le=128)
-    include_uppercase_letters: bool = True
-    include_lowercase_letters: bool = True
     include_numbers: bool = True
     include_symbols: bool = True
 
